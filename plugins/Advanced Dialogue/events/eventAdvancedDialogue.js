6d6087d59057949e266c5689214c627fc30bec8c
Pau Tomàs <pau.tomas@gmail.com>
2022-08-24 22:30:28 +0100

    Advance Dialogue: Render on Top

--- plugins/Advanced Dialogue/events/eventAdvancedDialogue.js
+++ plugins/Advanced Dialogue/events/eventAdvancedDialogue.js
@@ -146,6 +146,18 @@ const fields = [].concat(
         },
       ],
     },
+    {
+      key: `renderOnTop`,
+      label: "Render On Top (doesn't work in scenes with parallax)",
+      type: "checkbox",
+      defaultValue: true,
+      conditions: [
+        {
+          key: "__scriptTabs",
+          in: ["layout"],
+        },
+      ],
+    },
   ],
   // Text tab
   [
@@ -197,6 +209,7 @@ const compile = (input, helpers) => {
   const textX = input.textX === null ? 1 : input.textX;
   const textY = input.textY === null ? 1 : input.textY;
   const textHeight = input.textHeight === null ? 3 : input.textHeight;
+  const renderOnTop = input.renderOnTop;
 
   console.log(input);
   console.log(maxHeight, minHeight, textX, textY, textHeight);
@@ -220,13 +233,22 @@ const compile = (input, helpers) => {
   console.log(minHeight, minNumLines, maxHeight);
 
   const textBoxHeight = Math.max(minNumLines, minHeight);
-  const textBoxY = 18 - textBoxHeight;
+  const textBoxY = renderOnTop ? 0 : 18 - textBoxHeight;
 
   const x = decOct(1 + textX + (avatarId ? 3 : 0));
   const y = decOct(1 + textY);
   const textPosSequence = `\\003\\${x}\\${y}`;
 
   _addComment("Advanced Text Dialogue");
+
+  if (renderOnTop) {
+    appendRaw(
+      `
+VM_PUSH_CONST 0
+VM_GET_UINT8 .ARG0, _overlay_cut_scanline
+VM_SET_CONST_UINT8 _overlay_cut_scanline, ${textBoxHeight * 8 - 1}`
+    );
+  }
 
   textInputs.forEach((text, textIndex) => {
     let avatarIndex = undefined;
@@ -275,36 +297,36 @@ const compile = (input, helpers) => {
 
     const isModal = input.closeWhen !== "notModal";
     if (isModal) {
-      const waitFlags = [".UI_WAIT_WINDOW"];
-      switch (input.closeWhen) {
-        case "text":
-          waitFlags.push(".UI_WAIT_TEXT");
-          break;
-        case "key":
-          console.log("BUTTON", input.closeButton);
-          if (input.closeButton === "a") {
-            waitFlags.push(".UI_WAIT_BTN_A");
-          }
-          if (input.closeButton === "b") {
-            waitFlags.push(".UI_WAIT_BTN_B");
-          }
-          if (input.closeButton === "any") {
-            waitFlags.push(".UI_WAIT_BTN_ANY");
-          }
-          break;
-        default:
-          break;
+      const waitFlags = [".UI_WAIT_WINDOW", ".UI_WAIT_TEXT"];
+      if (input.closeWhen === "key") {
+        console.log("BUTTON", input.closeButton);
+        if (input.closeButton === "a") {
+          waitFlags.push(".UI_WAIT_BTN_A");
+        }
+        if (input.closeButton === "b") {
+          waitFlags.push(".UI_WAIT_BTN_B");
+        }
+        if (input.closeButton === "any") {
+          waitFlags.push(".UI_WAIT_BTN_ANY");
+        }
       }
       _overlayWait(isModal, waitFlags);
     }
 
     if (isModal && textIndex === textInputs.length - 1) {
       _overlayMoveTo(0, 18, speedOut);
-      _overlayWait(isModal, [".UI_WAIT_WINDOW"]);
-    } else {
       _overlayWait(isModal, [".UI_WAIT_WINDOW", ".UI_WAIT_TEXT"]);
     }
   });
+
+  if (renderOnTop) {
+    _overlayMoveTo(0, 18, speedOut);
+    appendRaw(`
+VM_IDLE
+VM_SET_UINT8 _overlay_cut_scanline, .ARG0
+VM_POP 1`);
+  }
+
   _addNL();
 };
 
