71e0ecd64ccb96690e418567ccf7bc782032e81c
Pau Tomàs <pau.tomas@gmail.com>
2022-08-26 09:08:01 +0100

    Properly handle printer errors

--- plugins/Printer/events/eventPrintScreenBackground.js
+++ plugins/Printer/events/eventPrintScreenBackground.js
@@ -24,7 +24,6 @@ const compile = (input, helpers) => {
     getNextLabel,
     _declareLocal,
     _addComment,
-    _ifConst,
     _compilePath,
     _label,
     _addNL,
@@ -32,46 +31,54 @@ const compile = (input, helpers) => {
 
   _addComment("Print screen background");
 
-  const hasPrintErrorRef = _declareLocal("print_error", 1, true);
+  const printStatusRef = _declareLocal("print_status", 1, true);
   const printSuccesfulLabel = getNextLabel();
 
-  appendRaw(`
+  appendRaw(`; Copy the background tiles to the overlay
 VM_PUSH_CONST 0
 VM_PUSH_CONST 0
 VM_GET_INT16  .ARG1, _scroll_x
 VM_GET_INT16  .ARG0, _scroll_y      
 
 VM_RPN
-.R_INT8 0
-.R_INT8 0
-.R_INT8 20
-.R_INT8 18
+  .R_INT8 0
+  .R_INT8 0
+  .R_INT8 20
+  .R_INT8 18
 
-.R_INT8 0
-.R_REF  .ARG1
-.R_INT16 8
-.R_OPERATOR  .DIV
-.R_OPERATOR .MAX
+  .R_INT8 0
+  .R_REF  .ARG1
+  .R_INT16 8
+  .R_OPERATOR  .DIV
+  .R_OPERATOR .MAX
 
-.R_INT8 0
-.R_REF  .ARG0
-.R_INT16 8
-.R_OPERATOR  .DIV
-.R_OPERATOR .MAX
+  .R_INT8 0
+  .R_REF  .ARG0
+  .R_INT16 8
+  .R_OPERATOR  .DIV
+  .R_OPERATOR .MAX
 
-.R_STOP
+  .R_STOP
 
 VM_OVERLAY_SET_SUBMAP_EX  .ARG5
 
 VM_POP  6
 `);
 
-  appendRaw(`
-VM_PRINTER_DETECT       ${hasPrintErrorRef}, 30
-VM_PRINT_OVERLAY        ${hasPrintErrorRef}, 0, 18
+  appendRaw(`; Detect printer and print
+VM_PRINTER_DETECT       ${printStatusRef}, 30
+VM_PRINT_OVERLAY        ${printStatusRef}, 0, 18
 `);
 
-  _ifConst(".EQ", hasPrintErrorRef, 0, printSuccesfulLabel, 0);
+  appendRaw(`; Check status for error code
+VM_RPN
+  .R_REF      ${printStatusRef}
+  .R_INT8     0xF0
+  .R_OPERATOR .B_AND
+  .R_STOP
+VM_IF_CONST             .EQ, .ARG0, 0, ${printSuccesfulLabel}, 1
+`);
+
   _addNL();
   _compilePath(input.true);
   _label(printSuccesfulLabel);
